b2c71382f1ad948d6af84d2f776bd1ac9a5b4445
HunteRoi <tinael.devresse@hotmail.com>
2020-05-22 18:12:37 +0200

    Add a picture and update deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -12,20 +12,21 @@ const e_x = emoji.get('x');
 (async () => {
   try {
     await execa('git', ['checkout', '--orphan', 'gh-pages']);
+
     console.log(`${e_arrows} ${chalk.yellow('Building...')}`);
     await execa('npm', ['run', 'build']);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync('dist') ? 'dist' : 'build';
     await execa('git', ['--work-tree', folderName, 'add', '--all']);
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
+
     console.log(`${e_arrows} ${chalk.yellow('Pushing...')}`);
     await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
+
     await execa('rm', ['-r', folderName]);
     await execa('git', ['checkout', '-f', 'master']);
     await execa('git', ['branch', '-D', 'gh-pages']);
-    console.log(
-      `${e_rocket} ${chalk.green('Successfully deployed')} ${e_rocket}`
-    );
+    console.log(`${e_rocket} ${chalk.green('Successfully deployed')} ${e_rocket}`);
   } catch (e) {
     console.log(`${e_x} ${chalk.red('An error occured')} ${e_x}\n${e.message}`);
     process.exit(1);
