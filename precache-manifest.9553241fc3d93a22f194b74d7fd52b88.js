f3db95f33b1bdf129817665417af8cfbaa926fa0
hunteroi <me@tinaeldevresse.eu>
2021-05-24 21:55:11 +0200

    gh-pages

---
+++ precache-manifest.9553241fc3d93a22f194b74d7fd52b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fce11bfa12f1f9386d882d06d6db3c",
+    "url": "/local-recipes/index.html"
+  },
+  {
+    "revision": "dab9b031a02ef58b5ec4",
+    "url": "/local-recipes/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6c7e35d6cdc119b232ca",
+    "url": "/local-recipes/static/js/2.b78458b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/local-recipes/static/js/2.b78458b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab9b031a02ef58b5ec4",
+    "url": "/local-recipes/static/js/main.3f8bbd3b.chunk.js"
+  },
+  {
+    "revision": "52a237c3e99678ba4d43",
+    "url": "/local-recipes/static/js/runtime-main.5081d524.js"
+  }
+]);
